e577398430375b12f0dc772b045c730aafb08732
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-10-16 17:18:02 -0400

    commit

---
+++ Projeto-pdm-vite/js/main.js
@@ -0,0 +1,36 @@
+//registrando a service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+        try {
+            let reg;
+            reg = await navigator
+                .serviceWorker
+                .register('./sw.js', {type: "module"});
+            console.log('Service worker registrada! reg');
+            postNews();
+        } catch (err) {
+            console.log(' Service worker registro falhou:', err);
+        }
+    });
+}
+const apiKey = 'de5956d4dcc9475186995d9019602ee8';
+let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${apiKey}`;
+const main = document.querySelector('main');
+
+async function postNews() {
+    const res = await fetch(url); const data = await res.json();
+    main.innerHTML = data.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) { 
+    console.log(article);
+    return `
+    <div class="article">
+    <a href="${article.url}" target="_blank">
+    <img src="${article.urlToImage}" class="image" alt="${article.content}"/>
+    <h2>${article.title}</h2>
+    <p>${article.description}</p>
+    </a>
+    </div>
+    `
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# projetopdm
+ atividade pdm 
